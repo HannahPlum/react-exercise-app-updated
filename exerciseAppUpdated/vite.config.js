c6b88554c4ddd67e229c9e107c004e4c259ebf02
HannahPlum <ha537956@ucf.edu.com>
2025-02-24 21:32:15 -0500

    had to change default export

--- exerciseAppUpdated/vite.config.js
+++ exerciseAppUpdated/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-exercise-app-updated/', // Update this to match your GitHub repo name
 })
